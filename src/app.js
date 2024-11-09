a172d6894276e4654b9381a9c18642cd669a7936
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-11-09 01:19:55 +0000

    Start of insult generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#ExcuseBtn").addEventListener("click", () => {
+    document.querySelector("#excuseLine").innerHTML = generateExcuse();
+  });
+  console.log("Excuse");
+};
+let generateExcuse = () => {
+  let pronoun = ["You"];
+  let insult = ["dirty", "ugly", "fart sniffing", "smelly"];
+  let secondInsult = ["no brained", "loser", "low life", "no shoe wearin'"];
+  let finalInsult = ["son uv ahhhhh", "gobermouch", "chicken", "airhead"];
+  // let where = ["in the closet", "under the bed", "in the pool"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let insultIndex = Math.floor(Math.random() * insult.length);
+  let secondInsultIndex = Math.floor(Math.random() * secondInsult.length);
+  let finalInsultIndex = Math.floor(Math.random() * finalInsult.length);
+  // let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    insult[insultIndex] +
+    " " +
+    secondInsult[secondInsultIndex] +
+    " " +
+    finalInsult[finalInsultIndex]
+    //  +
+    // " " +
+    // where[whereIndex]
+  );
 };
